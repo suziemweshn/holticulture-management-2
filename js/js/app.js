d3bbc2ff641fd02a12624102b0d73b39779fad90
suziemweshn <suziemweshn@gmail.com>
2023-05-16 09:28:08 +0300

    2nd commit

--- js/js/app.js
+++ js/js/app.js
@@ -1,10 +1,21 @@
 
+        function myFunction() {
+          // Declare variables
+          var input, filter, ul, li, a, i;
+          input = document.getElementById("mySearch");
+          filter = input.value.toUpperCase();
+          ul = document.getElementById("myMenu");
+          li = ul.getElementsByTagName("li");
+        
+          // Loop through all list items, and hide those who don't match the search query
+          for (i = 0; i < li.length; i++) {
+            a = li[i].getElementsByTagName("a")[0];
+            if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+              li[i].style.display = "hidden";
+            } else {
+              li[i].style.display = "hidden";
+            }
+          }
+        }
+  
 
-console.log("Hello world");
-
-document.querySelector('.text-primary').textContent="Welcome";
-function myFunction(){
-  window.alert("Welcome");
-       
-    
-}
