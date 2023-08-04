acbf9d532d053f038095fa76daa6daa61ea61e75
suziemweshn <suziemweshn@gmail.com>
2023-08-04 16:01:20 +0300

    added the changes

--- cart2.php
+++ cart2.php
@@ -38,39 +38,47 @@ $cartItems = $_SESSION['cart'] ?? [];
     <h1>Shopping Cart</h1>
 
     <?php if (count($cartItems) > 0): ?>
-        <table border="1">
-            <thead>
+    <table border="1">
+        <thead>
+            <tr>
+                <th>Product Name</th>
+                <th>Product Description</th>
+                <th>Product Price</th>
+                <th>Quantity</th>
+                <th>Subtotal</th>
+                <th>Product Image</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($cartItems as $item): ?>
                 <tr>
-                    <th>Product Name</th>
-                    <th>Product Description</th>
-                    <th>Product Price</th>
-                    <th>Quantity</th>
-                    <th>Subtotal</th>
-                    <th>Action</th>
+                    <td><?php echo $item['name']; ?></td>
+                    <td><?php echo $item['description']; ?></td>
+                    <td><?php echo $item['price']; ?></td>
+                    <td><?php echo $item['quantity']; ?></td>
+                    <td><?php echo $item['price'] * $item['quantity']; ?></td>
+                    <td>
+                        <?php if (!empty($item['image'])): ?>
+                            <img src="product-images/<?php echo $item['image']; ?>" alt="Product Image" width="100">
+                        <?php else: ?>
+                            No Image Available
+                        <?php endif; ?>
+                    </td>
+                    <td><a href="remove_from_cart.php?id=<?php echo $item['id']; ?>">Remove</a></td>
                 </tr>
-            </thead>
-            <tbody>
-                <?php foreach ($cartItems as $item): ?>
-                    <tr>
-                        <td><?php echo $item['name']; ?></td>
-                        <td><?php echo $item['description']; ?></td>
-                        <td><?php echo $item['price']; ?></td>
-                        <td><?php echo $item['quantity']; ?></td>
-                        <td><?php echo $item['price'] * $item['quantity']; ?></td>
-                        <td><a href="remove_from_cart.php?id=<?php echo $item['id']; ?>">Remove</a></td>
-                    </tr>
-                <?php endforeach; ?>
-            </tbody>
-        </table>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
 
-        <h3>Total Price: $<?php echo calculateTotalPrice($cartItems); ?></h3>
+    <h3>Total Price: $<?php echo calculateTotalPrice($cartItems); ?></h3>
 
-        <button onclick="clearCart()">Clear Cart</button>
-    <?php else: ?>
-        <p>Your cart is empty.</p>
-    <?php endif; ?>
+    <button onclick="clearCart()">Clear Cart</button>
+<?php else: ?>
+    <p>Your cart is empty.</p>
+<?php endif; ?>
 
-    <button onclick="closeCart()">Close Cart</button>
+<button onclick="closeCart()">Close Cart</button>
 
     <script>
         function clearCart() {
